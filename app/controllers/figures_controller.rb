22c32979a339bb1b9f5b5b1d1d61a802139eb092
Reno Sorensen <renojames19@gmail.com>
2019-06-09 20:17:45 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -51,4 +51,36 @@ class FiguresController < ApplicationController
     erb :"/figures/edit"
   end
 
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+
+    if !!params["figure"]["name"] && params["figure"]["name"] != ""
+      @figure.name = params["figure"]["name"]
+    end
+
+    if !!params["figure"]["title_ids"]
+      params["figure"]["title_ids"].each do |title_id|
+        @figure.titles << Title.find(title_id.to_i)
+      end
+    end
+
+    if !!params["figure"]["landmark_ids"]
+      params["figure"]["landmark_ids"].each do |landmark_id|
+        @figure.landmarks << Landmark.find(landmark_id.to_i)
+      end
+    end
+
+    if !!params["title"] && params["title"] != ""
+      @figure.titles << Title.create(name: params["title"])
+    end
+
+    if !!params["new_landmark"]["name"] && params["new_landmark"]["name"] != ""
+      @figure.landmarks << Landmark.create(name: params["new_landmark"]["name"], year_completed: params["new_landmark"]["year_completed"])
+    end
+
+    @figure.save
+
+    redirect "/figures/#{@figure.id}"
+  end
+
 end
